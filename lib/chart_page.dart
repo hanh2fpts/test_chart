9b5f0fa96d488c2e8a44a7ca9c2a45c005b322d2
hanh <hanh2@fpts.com.vn>
2023-07-20 17:02:06 +0700

    update code

--- lib/chart_page.dart
+++ lib/chart_page.dart
@@ -102,16 +102,153 @@ class _ChartPageState extends State<ChartPage> {
                   ),
                 ),
               ),
-              Container(
-                padding: const EdgeInsets.all(3),
-                margin: const EdgeInsets.all(5),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(7), border: Border.all(color: Colors.grey)),
-                child: SvgPicture.asset(
-                  'assets/icons/icon_fx.svg',
-                  height: 20,
-                  width: 20,
-                  color: Colors.grey.shade700,
+              GestureDetector(
+                onTap: () {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (context) {
+                      return Container(
+                          height: MediaQuery.of(context).size.height / 4,
+                          width: MediaQuery.of(context).size.width,
+                          decoration: const BoxDecoration(
+                              borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(5), topRight: Radius.circular(5))),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                            child: Column(
+                              children: [
+                                const Row(
+                                  children: [
+                                    Text('Các chỉ số'),
+                                    Spacer(),
+                                    Text(
+                                      'X',
+                                      style: TextStyle(color: Colors.blue, fontSize: 20),
+                                    )
+                                  ],
+                                ),
+                                Divider(
+                                  color: Colors.grey.shade300,
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    GestureDetector(
+                                      onTap: () {
+                                        // String scrip = "toggleVolumeLabel()";
+                                        // controller.runJavascript(scrip);
+                                        controller.runJavascript("removeStudies();");
+                                      },
+                                      child: Container(
+                                        height: 25,
+                                        width: 100,
+                                        decoration: BoxDecoration(
+                                            borderRadius:
+                                                const BorderRadius.all(Radius.circular(8)),
+                                            border: Border.all(color: Colors.grey.shade300)),
+                                        child: const Center(child: Text('Volume')),
+                                      ),
+                                    ),
+                                    Container(
+                                      height: 25,
+                                      width: 100,
+                                      decoration: BoxDecoration(
+                                          borderRadius: const BorderRadius.all(Radius.circular(8)),
+                                          border: Border.all(color: Colors.grey.shade300)),
+                                      child: const Center(child: Text('RSI')),
+                                    ),
+                                    Container(
+                                      height: 25,
+                                      width: 100,
+                                      decoration: BoxDecoration(
+                                          borderRadius: const BorderRadius.all(Radius.circular(8)),
+                                          border: Border.all(color: Colors.grey.shade300)),
+                                      child: const Center(child: Text('Bol. Bands')),
+                                    ),
+                                  ],
+                                ),
+                                Divider(
+                                  color: Colors.grey.shade300,
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Container(
+                                      height: 25,
+                                      width: 100,
+                                      decoration: BoxDecoration(
+                                          borderRadius: const BorderRadius.all(Radius.circular(8)),
+                                          border: Border.all(color: Colors.grey.shade300)),
+                                      child: const Center(child: Text('MA5')),
+                                    ),
+                                    Container(
+                                      height: 25,
+                                      width: 100,
+                                      decoration: BoxDecoration(
+                                          borderRadius: const BorderRadius.all(Radius.circular(8)),
+                                          border: Border.all(color: Colors.grey.shade300)),
+                                      child: const Center(child: Text('MA10')),
+                                    ),
+                                    Container(
+                                      height: 25,
+                                      width: 100,
+                                      decoration: BoxDecoration(
+                                          borderRadius: const BorderRadius.all(Radius.circular(8)),
+                                          border: Border.all(color: Colors.grey.shade300)),
+                                      child: const Center(child: Text('MA20')),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Container(
+                                      height: 25,
+                                      width: 100,
+                                      decoration: BoxDecoration(
+                                          borderRadius: const BorderRadius.all(Radius.circular(8)),
+                                          border: Border.all(color: Colors.grey.shade300)),
+                                      child: const Center(child: Text('MA50')),
+                                    ),
+                                    Container(
+                                      height: 25,
+                                      width: 100,
+                                      decoration: BoxDecoration(
+                                          borderRadius: const BorderRadius.all(Radius.circular(8)),
+                                          border: Border.all(color: Colors.grey.shade300)),
+                                      child: const Center(child: Text('MA100')),
+                                    ),
+                                    Container(
+                                      height: 25,
+                                      width: 100,
+                                      decoration: BoxDecoration(
+                                          borderRadius: const BorderRadius.all(Radius.circular(8)),
+                                          border: Border.all(color: Colors.grey.shade300)),
+                                      child: const Center(child: Text('MA200')),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ));
+                    },
+                  );
+                },
+                child: Container(
+                  padding: const EdgeInsets.all(3),
+                  margin: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(7),
+                      border: Border.all(color: Colors.grey)),
+                  child: SvgPicture.asset(
+                    'assets/icons/icon_fx.svg',
+                    height: 20,
+                    width: 20,
+                    color: Colors.grey.shade700,
+                  ),
                 ),
               ),
               const Spacer(),
